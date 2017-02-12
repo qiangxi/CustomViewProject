dac87666d913ea19c35965f1f28f2ac4f60d3624
qiangxi <981787371@qq.com>
2017-02-12 18:48:20 +0800

    新增DashView

--- app/src/main/java/com/lanma/customviewproject/views/ScrapeView.java
+++ app/src/main/java/com/lanma/customviewproject/views/ScrapeView.java
@@ -14,7 +14,6 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
